f520ba1f7c7d7a30aedb842c5688be21eca3bdb0
Darshana Miraje <darshanamiraje01@gmail.com>
2026-05-25 22:41:08 +0530

    Implement JPA repositories

---
+++ src/main/java/com/example/employee_management_system/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.example.employee_management_system.repository;
+
+import com.example.employee_management_system.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findBySalaryGreaterThan(Double salary);
+}
